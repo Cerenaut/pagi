b8bb8eb053356588cb48479b0ab1d3f5abea4bbe
Abdelrahman Ahmed <181392+abdel@users.noreply.github.com>
2019-08-02 13:17:47 +1000

    Add inhibition to Sparse Conv AE

--- pagi/components/sparse_conv_autoencoder_component.py
+++ pagi/components/sparse_conv_autoencoder_component.py
@@ -32,13 +32,45 @@ class SparseConvAutoencoderComponent(ConvAutoencoderComponent):
   sparsening.
   """
 
+  inhibition = 'inhibition'
+
   @staticmethod
   def default_hparams():
     """Builds an HParam object with default hyperparameters."""
     hparams = ConvAutoencoderComponent.default_hparams()
     hparams.add_hparam('sparsity', 5)
     hparams.add_hparam('sparsity_output_factor', 1.5)
+    hparams.add_hparam('inhibition_decay', 0.0)
     return hparams
+
+  def _apply_inhibition(self, encoding):
+    """
+    Retard the activation of cells that fired recently.
+    inhibition shape = [b, h, w, f]
+    """
+    # Refractory period
+    # Inhibition is max (1) and decays to min (0)
+    # So refractory weight is 1-1=0, returning to 1 over time.
+    inhibition_pl = self._dual.get_pl(self.inhibition)
+    refractory = 1.0 - inhibition_pl  # ie inh.=1, ref.=0 . Inverted
+
+    # Pos encoding (only used for ranking)
+    # Find min in each batch sample
+    # This is wrong for conv.? Should be min in each conv location.
+    # But - it doesn't matter?
+    min_encoding = tf.reduce_min(encoding, axis=[1, 2, 3], keepdims=True)  # may be negative
+    pos_encoding = (encoding - min_encoding) + 1.0  # shift to +ve range, ensure min value is nonzero
+
+    # Apply inhibition/refraction
+    refracted_encoding = pos_encoding * refractory
+
+    return refracted_encoding
+
+  def _update_inhibition(self, training_mask):
+    inhibition_pl = self._dual.get_pl(self.inhibition)
+    inhibition = inhibition_pl * self._hparams.inhibition_decay # decay old inh
+    inhibition = tf.maximum(training_mask, inhibition)  # this should be per batch sample not only per dend
+    self._dual.set_op(self.inhibition, inhibition)
 
   def _build_filtering(self, training_encoding, testing_encoding):
     """Build filtering/masking for specified encoding."""
@@ -48,8 +80,14 @@ class SparseConvAutoencoderComponent(ConvAutoencoderComponent):
     batch_size = self._hparams.batch_size
 
     encoding_shape = training_encoding.get_shape().as_list()
+
     h = encoding_shape[1]
     w = encoding_shape[2]
+
+    self._dual.add(self.inhibition, shape=encoding_shape, default_value=0.0).add_pl()
+
+    top_k_input = self._apply_inhibition(top_k_input)
+    top_k2_input = self._apply_inhibition(top_k2_input)
 
     # Find the "winners". The top k elements in each batch sample. this is
     # what top_k does.
@@ -73,4 +111,27 @@ class SparseConvAutoencoderComponent(ConvAutoencoderComponent):
     training_filtered = training_encoding * either_mask # apply mask 3 to output 2
     testing_filtered = testing_encoding * top_k2_mask
 
+    self._update_inhibition(either_mask)
+
     return training_filtered, testing_filtered
+
+  def update_feed_dict(self, feed_dict, batch_type='training'):
+    """Update the feed dict."""
+    names = [self.inhibition]
+    self._dual.update_feed_dict(feed_dict, names)
+
+    super().update_feed_dict(feed_dict, batch_type)
+
+  def add_fetches(self, fetches, batch_type='training'):
+    """Adds ops that will get evaluated."""
+    names = [self.inhibition]
+    self._dual.add_fetches(fetches, names)
+
+    super().add_fetches(fetches, batch_type)
+
+  def set_fetches(self, fetched, batch_type='training'):
+    """Store updated tensors"""
+    names = [self.inhibition]
+    self._dual.set_fetches(fetched, names)
+
+    super().set_fetches(fetched, batch_type)
